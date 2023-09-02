7554531eb653ee3797b0324ec5239a1858a4b92d
Tze Sheng Lee <kuansonlts@gmail.com>
2023-09-02 16:41:30 +0800

    Add react-storefront files

---
+++ react-storefront/cypress/elements/navigation.ts
@@ -0,0 +1,7 @@
+export const NAVIGATION = {
+  categoriesListButtons: "[data-testid*='categoriesList']",
+  searchIcon: "[data-testid='searchIcon']",
+  cartIcon: "[data-testid='cartIcon']",
+  cartCounter: "[data-testid='cartCounter']",
+  userIcon: "[data-testid='userIcon']",
+};
